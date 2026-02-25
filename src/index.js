7841e502454fe7e83d96c5280692bd0bb4bc55b4
wlewis0991 <wlewis1990@gmail.com>
2026-02-25 10:10:18 -0800

    Pseudo code of basic functions added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Main JS index
+// Battleship
+
+// Create Ship class
+// Should create shipes right at the start, track hits, and if its been sunk.
+//
+//
+// Create a Gameboard class
+// Board needs cords to know where ships are and also if attack was made there.
+// Board displays all attack and if shipsare hit/ sunk/ game over.
+//
+//
+//Create players
+//Creation of both a real and computer player.
+//Should track which players turn it is
+

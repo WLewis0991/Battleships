987b37a294766273832947e6d9d526cbfadb8f72
wlewis0991 <wlewis1990@gmail.com>
2026-02-25 16:48:26 -0800

    Notes edited and coordinates of ships saved in class

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import { Gameboard } from "./board.js";
 
 const board = new Gameboard();
 
-// Create a Gameboard class
-// Board needs cords to know swhere ships are and also if attack was made there.
-// Board displays all attack and if shipsare hit/ sunk/ game over.
-
-//Create players
-//Creation of both a real and computer player.
-//Should track which players turn it is
-
 console.log(board);
 console.log(board.placeAllShips());
 console.log(board);

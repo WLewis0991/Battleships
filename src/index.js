46decf9b879c496d048cd5844a9b267a0ec77b8a
wlewis0991 <wlewis1990@gmail.com>
2026-02-26 16:08:52 -0800

    Game shows 2 boards, only enemy board is clickable

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ button.addEventListener("click", () => {
 
 function buildBoard() {
 	const board = document.querySelector(".board");
+	const enemyBoard = document.querySelector(".enemyBoard");
 	board.innerHTML = "";
+	enemyBoard.innerHTML = "";
 
 	for (let i = 0; i < 10; i++) {
 		const row = document.createElement("div");
@@ -30,8 +32,21 @@ function buildBoard() {
 
 			row.appendChild(square);
 		}
-
 		board.appendChild(row);
+	}
+	for (let i = 0; i < 10; i++) {
+		const row = document.createElement("div");
+		row.classList.add("row");
+
+		for (let j = 0; j < 10; j++) {
+			const square = document.createElement("div");
+			square.classList.add("square");
+			square.dataset.x = i;
+			square.dataset.y = j;
+
+			row.appendChild(square);
+		}
+		enemyBoard.appendChild(row);
 	}
 }
 
